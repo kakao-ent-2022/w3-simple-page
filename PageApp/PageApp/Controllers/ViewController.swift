44a2e83b5c6d586b38ecb7dcff57d13bd4a95001
terry <terry.yes@kakaoent.com>
2022-01-24 11:26:20 +0900

    feat: 작품을 담을 Webtoon타입과 WebtoonViewModel 생성

---
+++ PageApp/PageApp/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PageApp
+//
+//  Created by terry.yes on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
