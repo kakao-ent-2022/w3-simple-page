64b3cb4520f91f59184c51e226fab7642b2b371a
terry <terry.yes@kakaoent.com>
2022-01-24 11:56:07 +0900

    feat: 테이블 뷰를 생성하여 데이터를 표시함
    - Basic cell 사용(Style: Subtitle)

--- PageApp/PageApp/Controllers/ViewController.swift
+++ PageApp/PageApp/Controllers/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let webtoonListViewModel = WebtoonListViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,3 +16,19 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        webtoonListViewModel.numberOfRowsInSection(section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WebtoonListCell", for: indexPath)
+        let webtoonViewModel = self.webtoonListViewModel.webtoonAtIndex(indexPath.row)
+        
+        cell.textLabel?.text = webtoonViewModel.title
+        cell.detailTextLabel?.text = webtoonViewModel.author
+        return cell
+    }
+    
+    
+}
