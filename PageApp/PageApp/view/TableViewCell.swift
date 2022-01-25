6b43861912361d545dc4737830b10f402ea703f5
nylah-j <nylah.j@kakaoent.com>
2022-01-25 14:13:54 +0900

    [FEAT] 웹툰 TableCell 깨지는 현상 해결
    - titleLabel의 trailing anchor를 button의 leading anchor보다 작거나 같게 위치하도록 만듬

--- PageApp/PageApp/view/TableViewCell.swift
+++ PageApp/PageApp/view/TableViewCell.swift
@@ -13,7 +13,6 @@ class TableViewCell: UITableViewCell {
     let thumbnailImageView = UIImageView()
     let purchaseButton = UIButton()
     
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         initUI()
@@ -40,11 +39,10 @@ class TableViewCell: UITableViewCell {
         contentView.addSubview(purchaseButton)
         contentView.addSubview(thumbnailImageView)
         
-        
+        initThumbnailImage()
+        initPurchaseButton()
         initTitleConstraint()
         initAuthorConstraint()
-        initPurchaseButton()
-        initThumbnailImage()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -55,27 +53,33 @@ class TableViewCell: UITableViewCell {
     
     func initTitleConstraint() {
         titleLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10).isActive = true
-        purchaseButton.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 10).isActive = true
+        titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: purchaseButton.leadingAnchor, constant: -10).isActive = true
         titleLabel.topAnchor.constraint(equalTo: thumbnailImageView.topAnchor).isActive = true
+        print(titleLabel.contentHuggingPriority(for: .horizontal))
+        
     }
     
     func initAuthorConstraint() {
-        authorLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10).isActive = true
+        authorLabel.leadingAnchor.constraint(equalTo:titleLabel.leadingAnchor).isActive = true
         authorLabel.bottomAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor).isActive = true
-        authorLabel.trailingAnchor.constraint(equalTo: purchaseButton.trailingAnchor).isActive = true
+        authorLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
     }
     
     func initPurchaseButton() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        
         purchaseButton.setTitle("구매", for: .normal)
         purchaseButton.setTitleColor(UIColor.blue, for: .normal)
         
-        contentView.trailingAnchor.constraint(equalTo: purchaseButton.trailingAnchor, constant: 20).isActive = true
-        contentView.centerYAnchor.constraint(equalTo: purchaseButton.centerYAnchor).isActive = true
+        purchaseButton.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+        
+        
+        safeArea.centerYAnchor.constraint(equalTo: purchaseButton.centerYAnchor).isActive = true
     }
     
     func initThumbnailImage() {
+        contentView.widthAnchor.constraint(equalTo: thumbnailImageView.widthAnchor, multiplier: 4).isActive = true
         thumbnailImageView.heightAnchor.constraint(equalTo: thumbnailImageView.widthAnchor).isActive = true
-        contentView.widthAnchor.constraint(equalTo: thumbnailImageView.widthAnchor, multiplier:  4).isActive = true
         
         contentView.topAnchor.constraint(equalTo: thumbnailImageView.topAnchor, constant: -10).isActive = true
         
